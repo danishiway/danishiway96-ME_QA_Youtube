5bf8584501af1abed25d8c34639756b64e06cfe1
Danish Faraz <danishiway96@gmail.com>
2024-05-28 23:55:47 +0530

    ME_QA_GoogleForm_MODULE_L

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -10,7 +10,6 @@ import java.util.logging.Level;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -20,7 +19,9 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -45,6 +46,7 @@ public class TestCases {
 
     
     driver.manage().window().maximize();
+    driver.get(url);
     ((WebDriver) driver).manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
     
@@ -59,19 +61,51 @@ public class TestCases {
 
     
     public  void testCase01(){
-    	boolean status= false;
-        System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        navigateToHomePage();
-      status = verifyHomePage();
-      System.out.println("Test Case 01 Status: " + (status ? "Passed" : "Failed"));
-      SelectAbout();
-      FlimTab();
-      musicTab();
-      NewsTab ();
-     
-        System.out.println("end Test case: testCase02");
+    	 System.out.println("Start Test case: testCase01");
+    	    driver.get(url);
+
+    	    boolean status = verifyHomePage();
+    	    assert status : "Test Case 01 Failed";
+    	    verifyAbout();
+    	    System.out.println("Test Case 01 Status: Passed");
+      
+      System.out.println("end Test of cases");
     }
+    
+    public  void testCase02(){
+     
+      System.out.println("Start Test case: testCase02");
+      navigateToHomePage(); 
+      clickTab("Movies");
+      clickScrollRightButtonMultipleTimes(3,"/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[2]/div[3]/ytd-shelf-renderer/div[1]/div[2]/yt-horizontal-list-renderer/div[3]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div");
+      WebElement movie = findMovie("//*[@id='items']/ytd-grid-movie-renderer[16]");
+      String maturityRating = getMaturityRating(movie);
+      assert maturityRating.contains("A") : "Maturity rating does not contain 'A'";
+      String genre = getGenre(movie);
+      assert genre.equalsIgnoreCase("Comedy") || genre.equalsIgnoreCase("Animation") : "Genre is not Comedy or Animation";
+      System.out.println("end Test of cases");
+    }
+    public  void testCase03(){
+    	 System.out.println("Start Test case: testCase03");
+    	navigateToHomePage(); 
+    	clickTab("Music");
+        clickScrollRightButtonMultipleTimes(3, "//*[@id='right-arrow']/ytd-button-renderer/yt-button-shape/button");
+        String playlistName = getPlaylistName("//*[@id='items']/ytd-compact-station-renderer[11]/div/a/h3");
+        System.out.println("Playlist Name: " + playlistName);
+        int numValue = getTrackCount("//*[@id='items']/ytd-compact-station-renderer[11]/div/a/p");
+        Assert.assertTrue(numValue <= 50, "numValue should be less than or equal to 50");
+        System.out.println("End Test of cases");
+    }
+    public  void testCase04(){
+    	navigateToHomePage();
+      
+      System.out.println("Start Test case: testCase04");
+      clickTab("News");
+      getNewsDetails();
+      
+      System.out.println("end Test of cases");
+    }
+    
     public void navigateToHomePage() {
         if (!driver.getCurrentUrl().equals(url)) {
             driver.get(url);
@@ -82,7 +116,7 @@ public class TestCases {
     public boolean verifyHomePage() {
         return driver.getCurrentUrl().equals(url);
     }
-public  void SelectAbout() {
+public  void verifyAbout() {
 	
 	WebElement aboutButton= driver.findElement(By.xpath("//a[contains(text(),'About')]"));
 	JavascriptExecutor js = (JavascriptExecutor)driver;
@@ -94,72 +128,60 @@ public  void SelectAbout() {
     String aboutText= driver.findElement(By.xpath("//*[@id='content']/section/p[1]")).getText();
     System.out.println("About Youtube :" +aboutText);
 }
-public void FlimTab() {
-	navigateToHomePage();
-	WebElement movieButton= driver.findElement(By.xpath("//a[@title='Movies']"));
-	JavascriptExecutor js = (JavascriptExecutor)driver;
-    
-	
-    js.executeScript("arguments[0].scrollIntoView();", movieButton);
-    movieButton.click();
-    
-   
-    WebElement scrollRightButton = driver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[2]/div[3]/ytd-shelf-renderer/div[1]/div[2]/yt-horizontal-list-renderer/div[3]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div"));
-    scrollRightButton.click();
-    scrollRightButton.click();
-    scrollRightButton.click();
+private WebElement findMovie(String path) {
+    return driver.findElement(By.xpath(path));
+}
+
+private String getMaturityRating(WebElement movie) {
+    return movie.findElement(By.xpath("./ytd-badge-supported-renderer/div[2]")).getText();
+}
+
+
+private String getGenre(WebElement movie) {
+    return movie.findElement(By.xpath(".//span[@class='grid-movie-renderer-metadata style-scope ytd-grid-movie-renderer']")).getText();
+}
+
 
     
-    WebElement movie = driver.findElement(By.xpath("//*[@id='items']/ytd-grid-movie-renderer[16]"));
-    String maturityRating = driver.findElement(By.xpath("//*[@id='items']/ytd-grid-movie-renderer[16]/ytd-badge-supported-renderer/div[2]")).getText();
-    if(maturityRating.contains("A")) {
-    	System.out.println("A is marked");
+
+// Method to click a tab by its title
+public void clickTab(String title) {
+    WebElement tab = driver.findElement(By.xpath("//a[@title='" + title + "']"));
+    tab.click();
+}
+
+// Method to click the scroll right button multiple times
+public void clickScrollRightButtonMultipleTimes(int times, String path) {
+    WebElement scrollRightButton =  driver.findElement(By.xpath(path));
+    for (int i = 0; i < times; i++) {
+        scrollRightButton.click();
     }
-
-    
-    WebElement movieGenre = movie.findElement(By.xpath(".//span[@class='grid-movie-renderer-metadata style-scope ytd-grid-movie-renderer']"));
-    String genre = movieGenre.getText();
-   if(genre.equalsIgnoreCase("Comedy") || genre.equalsIgnoreCase("Animation")) {
-	   System.out.println("Comedy cato");
-   }
-    
-}
-    
-
-public void musicTab() {
-	navigateToHomePage();
-	  WebElement musicTab = driver.findElement(By.xpath("//a[@title='Music']"));
-      musicTab.click();
-
-      
-      WebElement scrollRightButton = driver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[1]/div[3]/ytd-shelf-renderer/div[1]/div[2]/yt-horizontal-list-renderer/div[3]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div"));
-      scrollRightButton.click();
-      scrollRightButton.click();
-      scrollRightButton.click();
-
-      
-      String playlistName = driver.findElement(By.xpath("//*[@id='items']/ytd-compact-station-renderer[11]/div/a/h3")).getText();
-     
-      System.out.println("Playlist Name: " + playlistName);
-
-      String trackCount = ((WebElement) driver.findElement(By.xpath("//*[@id='items']/ytd-compact-station-renderer[11]/div/a/p"))).getText();
-      String  trackValue= trackCount.replaceAll("\\D+", "");
-
-      
-      int numValue = Integer.parseInt(trackValue);
-      
-   if(numValue <= 50) {
-	   System.out.println("musicTrack");
-   }
 }
 
-public void NewsTab () {
-	navigateToHomePage();
-	 WebElement newsTab = driver.findElement(By.xpath("//a[@title='News']"));
-     newsTab.click();
+// Method to get playlist name
+public String getPlaylistName(String path) {
+    WebElement playlist =  driver.findElement(By.xpath(path));
+    return playlist.getText();
+}
 
-   
+// Method to get track count
+public int getTrackCount(String path) {
+    WebElement trackCountElement;
+    String trackCount = "0";
+    
+    try {
+        trackCountElement = driver.findElement(By.xpath(path));
+        trackCount = trackCountElement.getText().replaceAll("\\D+", "");
+    } catch (Exception e) {
+        // Handle exception, e.g., log it or throw a custom exception
+        e.printStackTrace();
+    }
+    
+    return Integer.parseInt(trackCount);
+}
 
+public void getNewsDetails () {
+	
      for (int i = 1; i <= 3; i++) {
          String postTitle = driver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-section-renderer[2]/div/ytd-rich-shelf-renderer/div[1]/div[2]/ytd-rich-item-renderer["+i+"]/div/ytd-post-renderer/div[1]/div[1]/div[2]/a/span")).getText();
          String postBody = driver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-section-renderer[2]/div/ytd-rich-shelf-renderer/div[1]/div[2]/ytd-rich-item-renderer[\"+i+\"]/div/ytd-post-renderer/div[1]/div[2]/div[1]/yt-formatted-string/span")).getText();
@@ -178,51 +200,6 @@ public void NewsTab () {
 
      System.out.println("Total Likes on 3 Posts: " + totalLikes);
 }
-
-			 public void YoutubeViewSum () {
-			        try (FileInputStream fis = new FileInputStream("C://Users//TALHA ABDUL KHADER//eclipse-workspace//selenium-starter-2-main//qa_codeathon.xlsx");
-			             Workbook workbook = new XSSFWorkbook(fis)) {
-
-			            Sheet sheet = workbook.getSheetAt(0);
-			            Iterator<Row> iterator = sheet.iterator();
-
-			            while (iterator.hasNext()) {
-			                Row currentRow = iterator.next();
-			                String searchTerm = currentRow.getCell(0).getStringCellValue();
-			                searchAndScroll(searchTerm);
-			            }
-			        } catch (IOException e) {
-			            e.printStackTrace();
-			        }
-			    }
-			    private void searchAndScroll(String searchTerm){
-			        WebElement searchBox = driver.findElement(By.name("search_query"));
-			        searchBox.sendKeys(searchTerm);
-			        searchBox.submit();
-			        long totalViews = 0;
-			        while (totalViews < 100_000_000) { 
-			            scrollDown();
-			            
-			            List<WebElement> videoViewsElements = driver.findElements(By.xpath("//div[@id='metadata-line']//span[contains(text(),'views')]"));
-			            for (WebElement element : videoViewsElements) {
-			                String viewsText = element.getText();
-			                long views = extractViews(viewsText);
-			                totalViews += views;
-			            }
-			        
-			        }
-			    }
-
-			    private void scrollDown() {
-			        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-			        jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-			        
-			    }
-
-			    private long extractViews(String viewsText) {
-			       
-			        return 0; 
-			    }
 
 
 
